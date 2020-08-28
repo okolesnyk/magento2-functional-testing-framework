9122b44132eace55cdc7ecf96fcf84b4a818a32e
Ji Lu <jilu1@adobe.com>
2020-08-27 20:59:44 -0500

    MQE-2006: Create feature for before/after DB snapshot validation

--- src/Magento/FunctionalTestingFramework/Util/Sorter/ParallelGroupSorter.php
+++ src/Magento/FunctionalTestingFramework/Util/Sorter/ParallelGroupSorter.php
@@ -53,11 +53,12 @@ class ParallelGroupSorter
         $testNameToSizeForUse = $testNameToSize;
         $nodeNumber = 1;
 
+        /*
         foreach ($splitSuiteNamesToSize as $testName => $testSize) {
             $testGroups[$nodeNumber] = [$testName => $testSize];
             $nodeNumber++;
         }
-
+        */
         foreach ($testNameToSize as $testName => $testSize) {
             if (!array_key_exists($testName, $testNameToSizeForUse)) {
                 // skip tests which have already been added to a group
